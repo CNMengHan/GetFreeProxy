e5cf70c336563b0d5845635d5add3b3be6fa7f7b
CandyCub <120065153+CNMengHan@users.noreply.github.com>
2024-09-16 11:36:54 +0800

    Update GetFreeProxy.py

--- GetFreeProxy.py
+++ GetFreeProxy.py
@@ -32,7 +32,25 @@ proxy_list = [
     {"ip": "182.92.185.192", "port": 443, "type": "socks5"},
     {"ip": "8.140.255.223", "port": 443, "type": "socks4"},
     {"ip": "112.126.73.130", "port": 1080, "type": "socks5"},
-    {"ip": "117.80.63.66", "port": 1080, "type": "socks5"}
+    {"ip": "117.80.63.66", "port": 1080, "type": "socks5"},
+    {"ip": "150.158.2.130", "port": 80, "type": "http"},
+    {"ip": "111.230.53.150", "port": 3129, "type": "http"},
+    {"ip": "47.106.228.234", "port": 8088, "type": "socks5"},
+    {"ip": "58.57.130.134", "port": 1080, "type": "socks5"},
+    {"ip": "101.200.122.251", "port": 443, "type": "socks5"},
+    {"ip": "49.232.207.118", "port": 80, "type": "http"},
+    {"ip": "111.6.43.154", "port": 3128, "type": "http"},
+    {"ip": "123.249.77.88", "port": 80, "type": "socks4"},
+    {"ip": "121.37.221.98", "port": 443, "type": "socks5"},
+    {"ip": "120.25.222.150", "port": 1080, "type": "socks5"},
+    {"ip": "116.204.83.23", "port": 80, "type": "http"},
+    {"ip": "124.221.31.66", "port": 80, "type": "http"},
+    {"ip": "106.52.220.140", "port": 80, "type": "http"},
+    {"ip": "47.109.143.104", "port": 8010, "type": "socks5"},
+    {"ip": "159.75.179.68", "port": 8888, "type": "http"},
+    {"ip": "39.104.173.190", "port": 10057, "type": "socks4"},
+    {"ip": "49.232.61.105", "port": 1080, "type": "socks5"},
+    {"ip": "101.201.181.233", "port": 8000, "type": "socks5"}
 ]
 url = "https://www.uu-proxy.com/api/free"
 headers = {
